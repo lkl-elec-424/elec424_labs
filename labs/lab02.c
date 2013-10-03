8c21091b7632494b80e5d0d70386822e0738095a
Lindsay <lindsay@linuxmint.linuxmint-domain>
2013-10-02 23:02:02 -0500

    Fixed part 3 files, added part 2 files. The Makefile is currently wrong as it compiles for part 2.

--- labs/lab02.c
+++ labs/lab02.c
@@ -10,24 +10,30 @@
 
 /* Global var declarations */
 // Defined in lab2.h, used for getting PID status of all 4 motors
-MotorSpeeds motor_speeds; 
-TIM_TimeBaseInitStruct timebase_init_struct; // Initial struct for timer modules
+//MotorSpeeds motor_speeds; 
+TIM_TimeBaseInitTypeDef timebase_init_struct; // Initial struct for timer modules
 TIM_OCInitTypeDef timer_oc_init_struct;      // Initial struct for output compare timers
-bool pid_set_flag = false;					// Flag that is set to true when motor
+MotorSpeeds motor_speeds;
+uint16_t pid_set_flag = 0;					// Flag that is set to true when motor
+uint16_t capture = 0;       // When an interrupt was triggered
+uint16_t green_cnt = 0;     // Counter for the green LED
 											// PIDs need to be scaled and set, false otherwise
 
 /* Function definitions */
 void config_timer(void);
 void config_gpio(void);
 void config_nvic(void);
+void TIM2_IRQHandler(void);
+void TIM1_IRQHandler(void);
 
 /* #defines for constants */
 #define PRESCALE			 1800   // Divides the 72MHz SYSCLK so timers run at 40KHz
+#define DELAY_10MS            400   // 10 ms at frequency of 40KHz
+#define DELAY_100MS          4000   // 100 ms at frequency of 40KHz
+#define DELAY_500MS         20000   // Half a second at 40KHz
 #define TIM_PERIOD			39999   // Timers count to 40000 - 1 and then reset
-#define DELAY_10MS			  400   // Corresponds to 10 ms at frequency of 40KHz
-#define DELAY_100MS			 4000   // Corresponds to 100 ms at frequency of 40KHz
-#define RED_PERIOD			 9999   // Half of the red led's 0.25Hz cycle
-#define GREEN_PERIOD		19999   // Half of the green led's 0.5Hz cycle
+#define RED_START			 9999   // The red LED's starting time
+#define GREEN_START			19999   // The green LED's starting time
 
 // Main function
 int main() {
@@ -39,7 +45,13 @@ int main() {
 	config_gpio();
 	config_nvic();
 	config_timer();
-	 
+	
+	while (1) {
+		if (pid_set_flag == 1) {
+		/* NEED PID SCALING CODE */
+		}
+	}
+ 
 	return 0;
 }
 
@@ -52,92 +64,95 @@ int main() {
  */
 void config_timer(void) {
 
-	// Initial values for all the used channels in TIM2 and TIM3
+	// Initial values for all the used channels in TIM2 and TIM1
 	// They are all offset to avoid interrupting each other in the course of the program
 	__IO uint16_t TIM2_CCR1_init = 100;  // Emergency
 	__IO uint16_t TIM2_CCR2_init = 300;  // Data
 	__IO uint16_t TIM2_CCR3_init = 400;  // Orientation
 	__IO uint16_t TIM2_CCR4_init = 900;  // PID
+	__IO uint16_t TIM1_CCR1_init = 1100; // Debug 
+	__IO uint16_t TIM1_CCR2_init = RED_START;  // Green LED
+	__IO uint16_t TIM1_CCR3_init = GREEN_START;  // Green LED
 
-	__IO uint16_t TIM5_CCR1_init = 1100; // Debug 
-	__IO uint16_t TIM5_CCR2_init = RED_PERIOD;  // Red LED
-	__IO uint16_t TIM5_CCR3_init = GREEN_PERIOD;  // Green LED
+	// Set up peripheral clock for timers
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); 	
+	RCC_APB2PeriphClockCmd(RCC_APB1Periph_TIM1, ENABLE); 	
 
-	// All of these configs are valid for TIM2 and TIM5
+	// All of these configs are valid for TIM2 and TIM1
 	timebase_init_struct.TIM_Period = TIM_PERIOD;
 	timebase_init_struct.TIM_Prescaler = 0;
 	timebase_init_struct.TIM_ClockDivision = 0;
 	timebase_init_struct.TIM_CounterMode = TIM_CounterMode_Up;
 
-	// Initialize the timebases for TIM2 and TIM5
+	// Initialize the timebases for TIM2 and TIM1
 	TIM_TimeBaseInit(TIM2, &timebase_init_struct);
-	TIM_TimeBaseInit(TIM5, &timebase_init_struct);
+	TIM_TimeBaseInit(TIM1, &timebase_init_struct);
 
-	// Configure prescalers for TIM2 and TIM5
+	// Configure prescalers for TIM2 and TIM1
 	TIM_PrescalerConfig(TIM2, PRESCALE, TIM_PSCReloadMode_Immediate);
-	TIM_PrescalerConfig(TIM5, PRESCALE, TIM_PSCReloadMode_Immediate);
+	TIM_PrescalerConfig(TIM1, PRESCALE, TIM_PSCReloadMode_Immediate);
 
-	// Configure output compare timing mode for channel 1 of TIM2 and TIM5
+	// Configure output compare timing mode for channel 1 of TIM2 and TIM1
 	timer_oc_init_struct.TIM_OCMode = TIM_OCMode_Timing;
-	timer_oc_init_struct.TIM_OutputState = TIMOutputState_Enable;
+	timer_oc_init_struct.TIM_OutputState = TIM_OutputState_Enable;
 	timer_oc_init_struct.TIM_Pulse = TIM2_CCR1_init;
 	timer_oc_init_struct.TIM_OCPolarity = TIM_OCPolarity_High;
 	TIM_OC1Init(TIM2, &timer_oc_init_struct);
 	TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Disable);
 
-	timer_oc_init_struct.TIM_Pulse = TIM5_CCR1_init;
-	TIM_OC1Init(TIM5, &timer_oc_init_struct);
-	TIM_OC1PreloadConfig(TIM5, TIM_OCPreload_Disable);
+	timer_oc_init_struct.TIM_Pulse = TIM1_CCR1_init;
+	TIM_OC1Init(TIM1, &timer_oc_init_struct);
+	TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Disable);
 
-	// Configure output compare timing mode for channel 2 of TIM2 and TIM5
+	// Configure output compare timing mode for channel 2 of TIM2 and TIM1
 	timer_oc_init_struct.TIM_OCMode = TIM_OCMode_Timing;
-	timer_oc_init_struct.TIM_OutputState = TIMOutputState_Enable;
+	timer_oc_init_struct.TIM_OutputState = TIM_OutputState_Enable;
 	timer_oc_init_struct.TIM_Pulse = TIM2_CCR2_init;
 	timer_oc_init_struct.TIM_OCPolarity = TIM_OCPolarity_High;
 	TIM_OC2Init(TIM2, &timer_oc_init_struct);
 	TIM_OC2PreloadConfig(TIM2, TIM_OCPreload_Disable);
 
-	timer_oc_init_struct.TIM_Pulse = TIM5_CCR2_init;
-	TIM_OC2Init(TIM5, &timer_oc_init_struct);
-	TIM_OC2PreloadConfig(TIM5, TIM_OCPreload_Disable);
+	timer_oc_init_struct.TIM_Pulse = TIM1_CCR2_init;
+	TIM_OC2Init(TIM1, &timer_oc_init_struct);
+	TIM_OC2PreloadConfig(TIM1, TIM_OCPreload_Disable);
 
-	// Configure output compare timing mode for channel 3 of TIM2 and TIM5
+	// Configure output compare timing mode for channel 3 of TIM2 and TIM1
 	timer_oc_init_struct.TIM_OCMode = TIM_OCMode_Timing;
-	timer_oc_init_struct.TIM_OutputState = TIMOutputState_Enable;
+	timer_oc_init_struct.TIM_OutputState = TIM_OutputState_Enable;
 	timer_oc_init_struct.TIM_Pulse = TIM2_CCR3_init;
 	timer_oc_init_struct.TIM_OCPolarity = TIM_OCPolarity_High;
 	TIM_OC3Init(TIM2, &timer_oc_init_struct);
 	TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Disable);
 
-	timer_oc_init_struct.TIM_Pulse = TIM5_CCR3_init;
-	TIM_OC3Init(TIM5, &timer_oc_init_struct);
-	TIM_OC3PreloadConfig(TIM5, TIM_OCPreload_Disable);
+	timer_oc_init_struct.TIM_Pulse = TIM1_CCR3_init;
+	TIM_OC3Init(TIM1, &timer_oc_init_struct);
+	TIM_OC3PreloadConfig(TIM1, TIM_OCPreload_Disable);
 
-	// Configure output compare timing mode for channel 4 of TIM2 and TIM5
+	// Configure output compare timing mode for channel 4 of TIM2 and TIM1
 	timer_oc_init_struct.TIM_OCMode = TIM_OCMode_Timing;
-	timer_oc_init_struct.TIM_OutputState = TIMOutputState_Enable;
+	timer_oc_init_struct.TIM_OutputState = TIM_OutputState_Enable;
 	timer_oc_init_struct.TIM_Pulse = TIM2_CCR4_init;
 	timer_oc_init_struct.TIM_OCPolarity = TIM_OCPolarity_High;
 	TIM_OC4Init(TIM2, &timer_oc_init_struct);
 	TIM_OC4PreloadConfig(TIM2, TIM_OCPreload_Disable);
 
 	// Only need 3 channels for timer 5
-	timer_oc_init_struct.TIM_OutputState = TIMOutputState_Disable;
-	timer_oc_init_struct.TIM_Pulse = TIM5_CCR3_init; // Unchanged because unused 
-	TIM_OC4Init(TIM5, &timer_oc_init_struct);
-	TIM_OC4PreloadConfig(TIM5, TIM_OCPreload_Disable);
+	timer_oc_init_struct.TIM_OutputState = TIM_OutputState_Disable;
+	timer_oc_init_struct.TIM_Pulse = TIM1_CCR3_init; // Unchanged because unused 
+	TIM_OC4Init(TIM1, &timer_oc_init_struct);
+	TIM_OC4PreloadConfig(TIM1, TIM_OCPreload_Disable);
 
 	// Enable the timer interrupts 
 	TIM_ITConfig(TIM2, TIM_IT_CC1 | TIM_IT_CC2 | TIM_IT_CC3 | TIM_IT_CC4, ENABLE);
-	TIM_ITConfig(TIM5, TIM_IT_CC1 | TIM_IT_CC2 | TIM_IT_CC3, ENABLE);
+	TIM_ITConfig(TIM1, TIM_IT_CC1 | TIM_IT_CC2 | TIM_IT_CC3, ENABLE);
 
 	// Enable the timer counters
 	TIM_Cmd(TIM2, ENABLE);
-	TIM_Cmd(TIM5, ENABLE);
+	TIM_Cmd(TIM1, ENABLE); 
 }
 
 /*
- * config_nvic - Configures the interrupt vector handler for TIM2 and TIM5
+ * config_nvic - Configures the interrupt vector handler for TIM2 and TIM1
  *
  * Inputs: None
  * 
@@ -145,23 +160,23 @@ void config_timer(void) {
  */
 void config_nvic(void) {
 
-NVIC_InitTypeDef nvic_init_struct;
+	NVIC_InitTypeDef nvic_init_struct;
 
-// Want interrupts to trigger from TIM2 and TIM5, and TIM2 should be the higher
-// priority because it controls timing for the important functions and TIM5 just
-// controls the LEDS
-// Setting up interrupts for timer 2
-nvic_init_struct.NVIC_IRQChannel = TIM2_IRQn;
-nvic_init_struct.NVIC_IRQChannelPreemptionPriority = 0;
-nvic_init_struct.NVIC_IRQChannelSubPriority = 1;
-nvic_init_struct.NVIC_IRQChannelCmd = ENABLE;
-NVIC_Init(&nvic_init_struct);
+	// Want interrupts to trigger from TIM2 and TIM1, and TIM2 should be the higher
+	// priority because it controls timing for the important functions and TIM1 just
+	// controls the LEDS
+	// Setting up interrupts for timer 2
+	nvic_init_struct.NVIC_IRQChannel = TIM2_IRQn;
+	nvic_init_struct.NVIC_IRQChannelPreemptionPriority = 0;
+	nvic_init_struct.NVIC_IRQChannelSubPriority = 1;
+	nvic_init_struct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&nvic_init_struct);
 
-// Setting up timer 5 interrupts
-nvic_init_struct.NVIC_IRQChannel = TIM5_IRQn;
-nvic_init_struct.NVIC_IRQChannelPreemptionPriority = 1;
-nvic_init_struct.NVIC_IRQChannelSubPriority = 2;
-NVIC_Init(&nvic_init_struct);
+	// Setting up timer 5 interrupts
+	nvic_init_struct.NVIC_IRQChannel = TIM1_IRQn;
+	nvic_init_struct.NVIC_IRQChannelPreemptionPriority = 1;
+	nvic_init_struct.NVIC_IRQChannelSubPriority = 2;
+	NVIC_Init(&nvic_init_struct);
 }
 
 /* 
@@ -174,12 +189,126 @@ NVIC_Init(&nvic_init_struct);
  */
 void config_gpio(void) {
 
-GPIO_InitTypeDef gpio_init_struct;
+	GPIO_InitTypeDef gpio_init_struct;
 
-// Pins PB4 and PB5 are attached to the LEDs
-gpio_init_struct.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5;
-gpio_init_struct.GPIO_Mode = GPIO_Mode_Out_PP;
-gpio_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
-GPIO_Init(GPIOB, &gpio_init_struct);
+	// Configure the GPIO peripheral clock
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+	// Disable JTAG so pin attached to red LED can be GPIO
+	GPIO_PinRemapConfig(GPIO_Remap_SWJ_NoJTRST, ENABLE);	
+
+	// Pins PB4 and PB5 are attached to the LEDs
+	gpio_init_struct.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5;
+	gpio_init_struct.GPIO_Mode = GPIO_Mode_Out_PP;
+	gpio_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &gpio_init_struct);
 
 }
+
+/*
+ * TIM2_IRQHandler - The interrupt handler for timer 2. There are 4 possible
+ *                   interrupt channels. 
+ *
+ * Inputs: None
+ * Outputs: None
+ * Modifies: Possibly extern global var pid_set_flag
+ */
+void TIM2_IRQHandler(void) {
+
+    // Channel 1 is the interrupt for the detectEmegency() function
+    if (TIM_GetITStatus(TIM2, TIM_IT_CC1) != RESET) {
+
+        // Clear the IRQ
+        TIM_ClearITPendingBit(TIM2, TIM_IT_CC1);
+        detectEmergency();
+
+        // Interrupt needs to trigger again in 10ms
+        capture  = TIM_GetCapture1(TIM2);
+        TIM_SetCompare1(TIM2, capture + DELAY_10MS);
+
+ // Channel 2 is the interrupt for refreshSensorData()
+    } else if (TIM_GetITStatus(TIM2, TIM_IT_CC2) != RESET) {
+
+        // Clear the IRQ
+        TIM_ClearITPendingBit(TIM2, TIM_IT_CC2);
+        refreshSensorData();
+
+        // Interrupt needs to trigger again in 100ms
+        capture  = TIM_GetCapture2(TIM2);
+        TIM_SetCompare2(TIM2, capture + DELAY_100MS);
+
+
+    // Channel 3 is the interupt for calculateOrientation()
+    } else if (TIM_GetITStatus(TIM2, TIM_IT_CC3) != RESET) {
+
+        // Clear the IRQ
+        TIM_ClearITPendingBit(TIM2, TIM_IT_CC2);
+        calculateOrientation();
+        // This function refreshes once a second, so no need to reset the
+        // compare register
+    // Channel 4 is the interrupt for updatePid()
+    } else {
+
+        // Clear the IRQ
+        TIM_ClearITPendingBit(TIM2, TIM_IT_CC4);
+
+        // This function returns new motor PIDs, which means the main
+        // program needs to do additional processing after the interrupt
+        updatePid(&motor_speeds);
+        pid_set_flag = 1;
+        // This function refreshes once a second, so no need to reset the
+        // compare register
+    }
+}
+
+/*
+ * TIM1_IRQHandler - The interrupt handler for timer 5. There are 3 possible
+ *                   interrupt channels. 
+ *
+ * Inputs: None
+ * Outputs: None
+ * Modifies: None
+ */
+void TIM1_IRQHandler(void) {
+
+    // Channel 1 is the interrupt for the logDebugInfo() function
+    if (TIM_GetITStatus(TIM1, TIM_IT_CC1) != RESET) {
+
+        // Clear the IRQ
+        TIM_ClearITPendingBit(TIM1, TIM_IT_CC1);
+        logDebugInfo();
+        /* 
+         * This function is supposed to happen "whenever there's time," so
+         * it can just repeat once a second.
+         */
+
+	// Channel 2 is the interrupt handler for the red LED
+    } else if (TIM_GetITStatus(TIM1, TIM_IT_CC2) != RESET) {
+
+        // Clear the IRQ
+        TIM_ClearITPendingBit(TIM1, TIM_IT_CC2);
+
+        // Toggle the red LED
+        GPIO_WriteBit(GPIOB, GPIO_Pin_4,
+             (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_4)));
+        // The red LED flashes at a frequency of 0.5 Hz, which means it needs
+        // to toggle every second. Don't reset the compare register.
+
+	// Channel 3 is the interrupt handler for the green LED
+    } else {
+        TIM_ClearITPendingBit(TIM1, TIM_IT_CC3);
+
+        // The green LED flashes at 0.25Hz, so it should only be toggled
+        // every other cycle
+        if (green_cnt > 0) {
+
+            // Toggle the green LED
+            GPIO_WriteBit(GPIOB, GPIO_Pin_5,
+                 (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_5)));
+            green_cnt = 0;
+        } else {
+            green_cnt++;
+        }
+    }
+}
+
