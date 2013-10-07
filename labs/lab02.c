c9a55955cda30be88212e5a9c62441973857be92
Lindsay <lindsay@linuxmint.linuxmint-domain>
2013-10-07 11:03:53 -0500

    Finsihed cleaning code

--- labs/lab02.c
+++ labs/lab02.c
@@ -3,17 +3,39 @@
 #include "stm32f10x_it.h"
 #include "lab2.h"
 #include "stm32f10x_conf.h"
-#include "system_stm32f10x.h"
 
+/* #Defines */
+#define MOTOR_PULSE_WIDTH 		24 			// PWM duty cycle width
+#define MOTOR_PERIOD			2400		// Total period for PWM
+#define SYS_CORE_CLK			72000000	// Clock speed of 72MHz
+#define INTERRUPT_PRIORITY_0	0x00		// Interrupt priority of 0
+#define DIV_10MS				100			// Sets SysTick interrupt to
+											// every 10 ms
+#define TIMING_DELAY			1000		// Intial value for TimingDelay
+#define DELAY_100MS				10			// Counts until 100ms have elapsed
+#define DELAY_SEC				100			// Counts until 1 second 
+#define DELAY_2SEC				200			// Counts until 2 seconds
+
+// These offsets are used to schdule the different tasks in the SysTick
+// IRQ handler.
+#define LED_OFFSET				0			// Offset for LEDs in task chooser
+#define DATA_OFFSET				4			// Offset for getting sensor data
+#define CALC_OFFSET				9			// Offset for orientation calc
+#define MOTOR_OFFSET			7			// Offset for getting and setting
+											// motor speeds
+#define DEBUG_OFFSET			1			// Offset for logging debug info
+
+
+/* Global variables */
 // Structures to initialize the motor timers
 TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
 TIM_OCInitTypeDef  TIM_OCInitStructure;
 
 // Pulse width for motors
-uint16_t CCR1_Val = 24;
+uint16_t CCR1_Val = MOTOR_PULSE_WIDTH;
 
 // Clock speed as set in sys_init()
-uint32_t SystemCoreClock = 72000000;
+uint32_t SystemCoreClock = SYS_CORE_CLK;
 
 // Motor speed variables
 uint16_t motor1_speed;
@@ -35,8 +57,8 @@ void setMotor2(uint16_t step);
 void setMotor3(uint16_t step);
 void setMotor4(uint16_t step);
 
-int main(void)
-{
+int main(void) {
+
 	// Initialize the system clock to 72 MHz
 	sys_init();
 
@@ -47,15 +69,15 @@ int main(void)
 	GPIO_Configuration();
 
 	// Start SysTick and configure it to request an interrupt every 10 ms
-	if (SysTick_Config(SystemCoreClock/100)) {
+	if (SysTick_Config(SystemCoreClock/DIV_10MS)) {
 			while(1);
 	}
 
 	// Set the SysTick interrupt to the highest priority
-	NVIC_SetPriority(SysTick_IRQn, 0x00);
+	NVIC_SetPriority(SysTick_IRQn, INTERRUPT_PRIORITY_0);
 
 	// Initialize the motor timers
-	TIM_TimeBaseStructure.TIM_Period = 2400;
+	TIM_TimeBaseStructure.TIM_Period = MOTOR_PERIOD;
 	TIM_TimeBaseStructure.TIM_Prescaler = (uint16_t)(SystemCoreClock/CCR1_Val)-1;
 	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
 	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
@@ -65,63 +87,40 @@ int main(void)
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
 
+	// Set initial value of TimingDelay
+	TimingDelay = TIMING_DELAY;
 
-	/* TIM configuration */
-	TimingDelay = 1000;
-//GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, ENABLE);
-  while(1)
-  {
-    /*if(TimingDelay % 10 == 0)
-      detectEmergency();
-    else if (TimingDelay % 100 == 4)
-      refreshSensorData();
-    else if (TimingDelay % 1000 == 9)
-      calculateOrientation();
-    else if (TimingDelay % 1000 == 7){
-      updatePid(&motor_speeds);
-      // Get motor speeds from the struct
-      motor1_speed = (uint16_t)motor_speeds.m1;
-      motor2_speed = (uint16_t)motor_speeds.m2;
-      motor3_speed = (uint16_t)motor_speeds.m3;
-      motor4_speed = (uint16_t)motor_speeds.m4;
-
-      // Set motors
-      setMotor1(CCR1_Val * motor1_speed);
-      setMotor2(CCR1_Val * motor2_speed);
-      setMotor3(CCR1_Val * motor3_speed);
-      setMotor4(CCR1_Val * motor4_speed);
-    }
-    else if (TimingDelay % 100 == 1){
-      logDebugInfo();
-
-	// Toggle the red LED
-    } */
-
-      /*if (TimingDelay % 2000 == 11) {
-      GPIO_WriteBit(GPIOB, GPIO_Pin_4,
-             (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_4)));
-
-	// Toggle the green LED
-} else*//*if (TimingDelay % 1000 == 13 && already_triggered == 0) {
-	    already_triggered = 1;
-      GPIO_WriteBit(GPIOB, GPIO_Pin_5,
-             (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_5)));
-	}*/
-	
-  }
+	// Stay here forever and wait for interrupts
+	while(1) {
+	}
 }
 
-
-
+/*
+ * void RCC_Configuration - Initial setup of the RCC for the various 
+ *						    timers and peripherals.
+ *
+ * Inputs: None
+ * Outputs: None
+ */
 void RCC_Configuration(void)
 {
    /* TIM3 and TIM4 clock enable */
    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3 |
                          RCC_APB1Periph_TIM4, ENABLE);
-    /* GPIOB clock enable */
+    /* GPIOB and AFIO clock enable */
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO, ENABLE);
 }
-
+ 
+/* 
+ * void GPIO_Configuration - Set up the functionality of the GPIO pins used.
+ * 							 The motor pins and the red LED pin are all their
+ *							 alternate functions, and the green LED pin is its
+ *							 primary function. Also disables the JTAG
+ *							 functionality of the red LED pin.
+ *
+ * Inputs - None
+ * Outputs - None
+ */  
 void GPIO_Configuration(void)
 {
 
@@ -133,7 +132,6 @@ void GPIO_Configuration(void)
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_8 |GPIO_Pin_9;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-
   GPIO_Init(GPIOB, &GPIO_InitStructure);
 
   //Configure pins for LEDs
@@ -141,33 +139,30 @@ void GPIO_Configuration(void)
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-
 }
 
+/* 
+ * void setMotor1 - Sets the pulse width for motor 1, allowing it to be 
+ *				 	turned off or turned on and set to a specified speed.
+ *
+ * Inputs - uint16_t step: The width of the pulse for the motor's PWM control.
+ * Outputs - None
+ */	
 void setMotor1(uint16_t step){
   TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
   TIM_OCInitStructure.TIM_Pulse = step;
 
   TIM_OC4Init(TIM3, &TIM_OCInitStructure);
   TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
-
-  /*TIM_OCInitStructure.TIM_Pulse = 0;
-
-  TIM_OC3Init(TIM3, &TIM_OCInitStructure);
-  TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
-
-  TIM_TimeBaseInit(TIM4, &timebase_init_struct);
-
-  TIM_OC4Init(TIM4, &TIM_OCInitStructure);
-  TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable);
-
-  TIM_OC3Init(TIM4, &TIM_OCInitStructure);
-  TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
-
-  Delay(1000);*/
 }
 
+/* 
+ * void setMotor2 - Sets the pulse width for motor 2, allowing it to be 
+ *				 	turned off or turned on and set to a specified speed.
+ *
+ * Inputs - uint16_t step: The width of the pulse for the motor's PWM control.
+ * Outputs - None
+ */	
 void setMotor2(uint16_t step){
   TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
   TIM_OCInitStructure.TIM_Pulse = step;
@@ -176,6 +171,13 @@ void setMotor2(uint16_t step){
   TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
 }
 
+/* 
+ * void setMotor3 - Sets the pulse width for motor 3, allowing it to be 
+ *				 	turned off or turned on and set to a specified speed.
+ *
+ * Inputs - uint16_t step: The width of the pulse for the motor's PWM control.
+ * Outputs - None
+ */	
 void setMotor3(uint16_t step){
   TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
   TIM_OCInitStructure.TIM_Pulse = step;
@@ -184,6 +186,13 @@ void setMotor3(uint16_t step){
   TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable);
 }
 
+/* 
+ * void setMotor4 - Sets the pulse width for motor 4, allowing it to be 
+ *				 	turned off or turned on and set to a specified speed.
+ *
+ * Inputs - uint16_t step: The width of the pulse for the motor's PWM control.
+ * Outputs - None
+ */	
 void setMotor4(uint16_t step){
   TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
   TIM_OCInitStructure.TIM_Pulse = step;
@@ -192,90 +201,75 @@ void setMotor4(uint16_t step){
   TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
 }
 
-
+/*
+ * void TimingDelay_Decrement - Decrements the TimingDelay counter, or wraps
+ *								it back to the highest counter value if
+ *								TimingDelay has hit 0.
+ * 
+ * Inputs - None
+ * Outputs - None
+ */
 void TimingDelay_Decrement(void)
 {
-  if(TimingDelay != 0x00)
-    {
+  if(TimingDelay != 0) {
       TimingDelay--;
-  }else {
-      TimingDelay = 999;
+  } else {
+		// The -1 is to avoid running the functions controlled by logic with
+		// a 0 offset twice in a row.
+      TimingDelay = TIMING_DELAY - 1;
   }
-  //already_triggered = 0;
 }
 
+/*
+ * void chooseTask - Chooses which task(s) to execute based on the current
+ *					 value of TimingDelay. Called from the SysTick IRQ handler.
+ *
+ * Inputs - None
+ * Outputs - None
+ */
 void chooseTask(void) {
 
-  detectEmergency();
-  if (TimingDelay % 10 == 4){
-    refreshSensorData();}
-  else if (TimingDelay % 100 == 9){
-    calculateOrientation();}
-  else if (TimingDelay % 100 == 7){
-    updatePid(&motor_speeds);
-    // Get motor speeds from the struct
-    motor1_speed = (uint16_t)motor_speeds.m1;
-    motor2_speed = (uint16_t)motor_speeds.m2;
-    motor3_speed = (uint16_t)motor_speeds.m3;
-    motor4_speed = (uint16_t)motor_speeds.m4;
+	// Interrupt happens every 10 ms, so detectEmergency() must happen 
+    // every time this function is called.
+	detectEmergency();
 
-    // Set motors
-    setMotor1(CCR1_Val * motor1_speed);
-    setMotor2(CCR1_Val * motor2_speed);
-    setMotor3(CCR1_Val * motor3_speed);
-    setMotor4(CCR1_Val * motor4_speed);
-  }
-  else if (TimingDelay % 10 == 1){
-    logDebugInfo();
-  }
+	// The offsets are all meant to be orthogonal to each other so that none
+	// of the tasks are delayed.
+	if (TimingDelay % DELAY_100MS == DATA_OFFSET){
+    	refreshSensorData();
+	}
+	else if (TimingDelay % DELAY_SEC == CALC_OFFSET){
+		calculateOrientation();
+	}
+	else if (TimingDelay % DELAY_SEC == MOTOR_OFFSET){
+		updatePid(&motor_speeds);
 
-  if(TimingDelay % 100 == 0) {
-              //already_triggered = 1;
+		// Get motor speeds from the struct
+		motor1_speed = (uint16_t)motor_speeds.m1;
+		motor2_speed = (uint16_t)motor_speeds.m2;
+		motor3_speed = (uint16_t)motor_speeds.m3;
+		motor4_speed = (uint16_t)motor_speeds.m4;
 
-       GPIO_WriteBit(GPIOB, GPIO_Pin_5,
-                      (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_5)));
-       //GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, DISABLE);
-       }
-  if (TimingDelay % 200 ==0){
-      // RED LED
-        GPIO_WriteBit(GPIOB, GPIO_Pin_4,
-                (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_4)));
-        //GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_SET);
-     }/*else{
-         GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_RESET);
+		// Set motors
+		setMotor1(CCR1_Val * motor1_speed);
+		setMotor2(CCR1_Val * motor2_speed);
+		setMotor3(CCR1_Val * motor3_speed);
+		setMotor4(CCR1_Val * motor4_speed);
+	}
+	else if (TimingDelay % DELAY_100MS == DEBUG_OFFSET){
+		logDebugInfo();
+	}
 
-     }*/
+	// Toggle the green LED
+	if(TimingDelay % DELAY_SEC == LED_OFFSET) {
+		GPIO_WriteBit(GPIOB, GPIO_Pin_5,
+			(BitAction)(1 - GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_5)));
+	}
 
+	// Toggle the red LED
+	if (TimingDelay % DELAY_2SEC == LED_OFFSET){
+			GPIO_WriteBit(GPIOB, GPIO_Pin_4,
+				(BitAction)(1 - GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_4)));
+	}
 }
 
-#ifdef  USE_FULL_ASSERT
-
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t* file, uint32_t line)
-{
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
-  /* Infinite loop */
-  while (1)
-  {
-  }
-}
-
-#endif
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
